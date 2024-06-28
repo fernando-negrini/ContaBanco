1af7450d2c01ec2e6aa5a4fe7f2e65971d4e18a1
Fernando Negrini <contato.fernando.negrini@gmail.com>
2024-06-27 22:56:43 -0300

    atualização

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        // Declaração de variáveis
+        int NumeroConta;
+        String NomeCliente;
+        String Agencia;
+        Double Saldo;
+        Saldo = 237.48;
+
+        // Sintaxe do código
+        Scanner entrada = new Scanner(System.in);
+        // Exibir as mensagens para nosso usuário e coletar os dados que preencherão o espaço das variáveis
+        System.out.println("Por favor digite o número da agencia: ");
+        Agencia = entrada.next();
+
+        System.out.println("Agora  digite o número da Conta: ");
+        NumeroConta = entrada.nextInt();
+
+        System.out.println("Por favor digite o seu nome completo: ");
+        NomeCliente = entrada.nextLine();
+
+       // exibindo mensagem para o cliente
+       System.out.println("Olá "+ NomeCliente + "! Obrigado por criar uma conta em nosso banco, sua agência é a : "+ Agencia + " , conta "+ NumeroConta+ " e seu saldo é de R$ "+Saldo+" já disponível para saque!!");
+
+        entrada.close();
+
     }
 }
